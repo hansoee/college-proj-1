0635c2d4d6a07676df2fa46d68995426339db716
sordadros <88931738+sordadros@users.noreply.github.com>
2021-11-08 07:57:32 +0700

    selsort, bubsort, insort

--- sorting.cpp
+++ sorting.cpp
@@ -7,7 +7,7 @@ void tukar(int &data1, int &data2){
     data2=temp;
 }
 
-void desc_inSort(int data[], int n){          //descending
+void desc_inSort(int data[], int n){          
     int j;
     cout<<"\nDescending Insertion Sort:\n";
     for(int i=1;i<n;i++){
@@ -19,7 +19,7 @@ void desc_inSort(int data[], int n){          //descending
     }
 }
 
-void asc_inSort(int data[], int n){          //ascending
+void asc_inSort(int data[], int n){          
     int j;
     cout<<"\nAscending Insertion Sort:\n";
     for(int i=1;i<n;i++){
@@ -28,6 +28,54 @@ void asc_inSort(int data[], int n){          //ascending
             tukar(data[j], data[j-1]);
             j=j-1;
         };        
+    }
+}
+
+void desc_selSort(int data[], int pod, int lim){         
+    cout<<"\nDescending Selection Sort:\n";
+    for(int i=pod; i<lim; i++){
+        int token=i; 
+        for(int j=i+1; j<lim; j++){
+            if(data[i]<data[j]){
+                token=j;
+                tukar(data[i], data[j]);
+            }
+        }   
+    }
+}
+
+void asc_selSort(int data[], int pod, int lim){         
+    cout<<"\nAscending Selection Sort:\n";
+    for(int i=pod; i<lim; i++){
+        int token=i; 
+        for(int j=i+1; j<lim; j++){
+            if(data[i]>data[j]){
+                token=j;
+                tukar(data[i], data[j]);
+            }
+        }   
+    }
+}
+
+void desc_bubSort(int arr[], int n){
+    cout<<"\nDescending Bubble Sort:\n";
+    for(int i=0; i<n-1; i++){              
+        for(int j=0; j<n-1; j++){
+            if(arr[j]<arr[j+1]){
+                tukar(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+void asc_bubSort(int arr[], int n){
+    cout<<"\nAscending Bubble Sort:\n";
+    for(int i=0; i<n-1; i++){              
+        for(int j=0; j<n-1; j++){
+            if(arr[j]>arr[j+1]){
+                tukar(arr[j],arr[j+1]);
+            }
+        }
     }
 }
 
@@ -42,7 +90,7 @@ int main(){
     int N;
     cout<<"Masukkan jumlah data: "; cin>>N;
     int arr[N];
-    cout<<"Masukkan data yang mau diurutkan: ";
+    cout<<"Masukkan data yang akan diurutkan: ";
     for(int i=0; i<N; i++){
         cin>>arr[i];
     }
@@ -51,6 +99,13 @@ int main(){
     display(arr,N);
     desc_inSort(arr, N);
     display(arr,N);
-    
+    asc_selSort(arr, 0, N);
+    display(arr,N);
+    desc_selSort(arr, 0, N);
+    display(arr,N);
+    asc_bubSort(arr, N);
+    display(arr,N);
+    desc_bubSort(arr, N);
+    display(arr,N);
     return 0;
 }
